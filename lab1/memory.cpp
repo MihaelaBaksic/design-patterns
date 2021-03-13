ffbe8baaf6c6b58cfbc0a3de6d75b8b9a2e1b4b8
MihaelaBaksic <mihaela.baksic@gmail.com>
2021-03-13 16:12:49 +0100

    Memory

---
+++ lab1/memory.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#pragma pack(push)
+#pragma pack(4) // default is 8
+
+class CoolClass{
+public:
+  virtual void set(int x){x_=x;};
+  virtual int get(){return x_;};
+private:
+  int x_;
+};
+
+class PlainOldClass{
+public:
+  void set(int x){x_=x;};
+  int get(){return x_;};
+private:
+  int x_;
+};
+
+int main(void){
+    std::cout << "Size of CoolClass : " << sizeof(CoolClass) << std::endl;
+    std::cout << "Size of PlainOldClass : " << sizeof(PlainOldClass) << std::endl;
+    std::cout << "Size of pointer "<< sizeof(int*) << std::endl;
+    
+}
+
+#pragma pack(pop)
