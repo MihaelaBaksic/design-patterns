1525f4e86c914fe203b4685797b377b8f697f9ff
MihaelaBaksic <mihaela.baksic@gmail.com>
2021-04-08 23:42:22 +0200

    Added check for empty iterables

--- lab2/task012/task2/mymax.cpp
+++ lab2/task012/task2/mymax.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <assert.h>
 
 using namespace std;
 
@@ -20,8 +21,14 @@ Iterator mymax(Iterator first, Iterator last, Predicate pred){
 int arr_int[] = { 1, 3, 5, 7, 4, 6, 9, 2, 0 };
 vector<int> vector_int = { 1, 3, 5, 7, 4, 6, 9, 2, 0 };
 set<int> set_int = { 1, 3, 5, 7, 4, 6, 9, 2, 0 };
+string arr_str[] = {"mali", "veliki", "nijenajveci"};
+vector<int> vector_empty;
 
 bool gt_int(int t, int o){
+    return t > o;
+}
+
+bool gt_string(string t, string o){
     return t > o;
 }
 
@@ -29,6 +36,11 @@ int main(void){
     const int* maxint = mymax(&arr_int[0], &arr_int[sizeof(arr_int)/sizeof(*arr_int)], gt_int);
     cout << *maxint <<"\n";
 
+    const string* maxstr = mymax(&arr_str[0], &arr_str[sizeof(arr_str)/sizeof(*arr_str)], gt_string);
+    cout << *maxstr <<"\n";
+
+    auto empty_max = mymax(vector_empty.begin(), vector_empty.end(), gt_int);
+    assert(empty_max == vector_empty.end());
 
     auto vector_max = mymax(vector_int.begin(), vector_int.end(), gt_int);
     cout << *vector_max <<"\n";
