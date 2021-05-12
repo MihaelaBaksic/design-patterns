cbecae2b900fe6bd35131358c51dd2d594a6895d
MihaelaBaksic <mihaela.baksic@gmail.com>
2021-05-12 14:05:40 +0200

    Main fix

--- lab3/c_factory/main.c
+++ lab3/c_factory/main.c
@@ -5,30 +5,29 @@
 
 typedef char const* (*PTRFUN)();
 
-struct Animal{
+typedef struct Animal{
   PTRFUN* vtable;
-  const char* name;
   // vtable entries:
   // 0: char const* name(void* this);
   // 1: char const* greet();
   // 2: char const* menu();
-};
-
-// parrots and tigers defined in respective dynamic libraries
+} Animal;
 
 // animalPrintGreeting and animalPrintMenu similar as in lab 1
-void animalPrintGreeting(struct Animal* a){
-    printf("%s pozdravlja: %s\n", a->name, a->vtable[0]());
+void animalPrintGreeting( Animal* a){
+    printf("%s pozdravlja: %s\n", a->vtable[0](a), a->vtable[1]());
 }
 
-void animalPrintMenu(struct Animal* a){
-    printf("%s voli %s\n", a->name, a->vtable[1]());
+void animalPrintMenu( Animal* a){
+    printf("%s voli %s\n", a->vtable[0](a), a->vtable[2]());
 }
 
 
 int main(int argc, char *argv[]){
-  for (int i=0; i<argc; ++i){
-    struct Animal* p=(struct Animal*)myfactory(argv[i], "Modrobradi");
+  for (int i=1; i<argc; ++i){
+    printf("%s\n", argv[i]);
+    Animal* p=( Animal*)myfactory(argv[i], "Modrobradi");
+
     if (!p){
       printf("Creation of plug-in object %s failed.\n", argv[i]);
       continue;
